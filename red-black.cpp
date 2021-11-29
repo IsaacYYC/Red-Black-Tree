5cc71418cf0711423f19934d6d2d855e05d1ec4c
javierseidel <javierseidel96@gmail.com>
2021-11-28 21:38:10 -0700

    single file for code base

--- red-black.cpp
+++ red-black.cpp
@@ -1,7 +1,44 @@
-
 #include <iostream>
 
+class Node {
+    private:
+    Node *parent;
+    bool colour;
+
+    public:
+    int data;
+    Node *left, *right;
+    
+
+    Node(int data){
+        data = 0;
+        colour = false;
+        parent = left = right = NULL;
+    }
+    ~Node() {	
+    delete left;
+	delete right;
+    }
+};
+
+class RedBlackTree{
+    private:
+        Node *root;
+    public:
+        RedBlackTree() {root = NULL;}
+        void inOrderTraversalHelper(Node);
+};
+
+void inOrderTraversalHelper(Node *root){
+    if (root == NULL)
+        return;
+
+    inOrderTraversalHelper(root->left);
+    inOrderTraversalHelper(root->right);
+}
+
 int main() {
-    std::cout << "This is just a test to configure my stuff";
+
+    
     return 0;
 }
